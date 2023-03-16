c5dd95135ee62ce6fe7556f8d3d174985c80c6ef
Mehrdad.sami <mehrdad.sami@digikala.com>
2023-03-16 13:37:58 +0330

    Make user model and migration and write test for validates

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
   gem 'rspec-rails', '~> 6.0.0'
   gem 'faker'
+  gem 'shoulda-matchers', '~> 5.0'
 end
 
 group :development do
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'validations' do
+    subject { User.new(name: "Name", email: 'test@gmail.com') }
+    it {is_expected.to validate_presence_of(:name)}
+    it {is_expected.to validate_uniqueness_of(:email)}
+  end
+end
