f1747beaedd6e78ccd917342ace7b32cc5787510
Mehrdad.sami <mehrdad.sami@digikala.com>
2023-03-17 22:41:02 +0330

    Add learning path into course

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,10 @@
 class Course < ApplicationRecord
   belongs_to :author, class_name: 'User'
+  belongs_to :learning_path, optional: true
 
   validates :name, presence: true
   validates :author, presence: true
+  validates :order_in_learning_path, presence: true, if: -> {learning_path.present?}
+  validates :order_in_learning_path, absence: true, unless: -> {learning_path.present?}
+  validates :learning_path, presence: true, if: -> {learning_path_id.present?}
 end
---
+++ db/migrate/20230316150156_add_learning_path_to_course.rb
@@ -0,0 +1,6 @@
+class AddLearningPathToCourse < ActiveRecord::Migration[6.1]
+  def change
+    add_column :courses, :order_in_learning_path, :integer, null: true
+    add_reference :courses, :learning_path, foreign_key: true
+  end
+end
