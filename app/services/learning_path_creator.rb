15873b3b3a655e37363a3f70bddda5f889b35d6f
Mehrdad.sami <mehrdad.sami@digikala.com>
2023-03-18 19:51:01 +0330

    Make a service to create learning path

---
+++ app/services/learning_path_creator.rb
@@ -0,0 +1,25 @@
+class LearningPathCreator
+  class CourseIsEmptyException < StandardError; end
+
+  def initialize(name:, courses:)
+    @name = name
+    @courses = courses
+  end
+
+  def call
+    raise CourseIsEmptyException if @courses.empty?
+    learning_path = LearningPath.create!(name: @name)
+    course_data = make_course_data(learning_path.id)
+    Course.update(course_data.keys, course_data.values)
+  end
+
+  private
+  def make_course_data(learning_path_id)
+    data = {}
+    @courses.each_with_index do |id, index|
+      data[id] = {order_in_learning_path: index, learning_path_id: learning_path_id}
+    end
+    data
+  end
+
+end
---
+++ spec/services/learning_path_creator_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe LearningPathCreator do
+  describe 'call' do
+    it "create a learning path with courses" do
+      courses = create_list(:course, 4)
+      params = {name: 'Test', courses: courses.map(&:id)}
+      described_class.new(**params).call
+      expect(LearningPath.last.courses.count).to eq 4
+    end
+
+    it 'raise exception if coures is empty' do
+      expect { described_class.new(**{name: 'Test', courses: []}).call }.to raise_error(described_class::CourseIsEmptyException)
+    end
+  end
+end
