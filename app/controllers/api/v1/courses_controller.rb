9458653252f9debc55ca0f9b5db6aa962d6db88a
Mehrdad.sami <mehrdad.sami@digikala.com>
2023-03-18 16:17:26 +0330

    implement destroy and update for course

--- app/controllers/api/v1/courses_controller.rb
+++ app/controllers/api/v1/courses_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::CoursesController < ApplicationController
+  before_action :find_course, only: [:show, :destroy, :update]
+
   def index
     courses = Course.all
     render json: CourseSerializer.new(courses.includes(:author))
@@ -10,11 +12,23 @@ class Api::V1::CoursesController < ApplicationController
   end
 
   def show
-    render json: CourseSerializer.new(Course.find(params[:id])).serializable_hash
+    render json: CourseSerializer.new(@course).serializable_hash
+  end
+
+  def destroy
+    @course.destroy
+  end
+
+  def update
+    @course.update!(course_params)
   end
 
   private
   def course_params
     params.require(:course).permit(:name, :author_id, :learning_path_id, :order_in_learning_path)
+  end
+
+  def find_course
+    @course = Course.find(params[:id])
   end
 end
--- spec/requests/api/v1/courses_spec.rb
+++ spec/requests/api/v1/courses_spec.rb
@@ -24,7 +24,7 @@ RSpec.describe "Api::V1::Courses", type: :request do
     end
   end
 
-  describe "GET /course/:id" do
+  describe "GET /courses/:id" do
     it "returns course with id " do
       course = create(:course)
       get '/api/v1/courses/' + course.id.to_s
@@ -35,6 +35,38 @@ RSpec.describe "Api::V1::Courses", type: :request do
     it "returns 404 if send invalid id" do
       get '/api/v1/courses/dummyId'
       expect(response).to have_http_status(404)
+    end
+  end
+
+  describe "DELETE /courses/:id" do
+    it 'destroy the course' do
+      course = create(:course)
+      delete "/api/v1/courses/#{course.id}"
+      expect(response).to have_http_status(204)
+      expect(Course.find_by_id(course.id)).to be nil
+    end
+  end
+
+  describe "PUT /courses/:id" do
+    it "updates course" do
+      course = create(:course)
+      user = create(:user)
+      learning_path = create(:learning_path)
+      params = {
+        name: 'New Name',
+        author_id: user.id,
+        learning_path_id: learning_path.id,
+        order_in_learning_path: 1
+      }
+
+      put "/api/v1/courses/#{course.id}", params: {course: params}
+
+      expect(response).to have_http_status(204)
+      course.reload
+      expect(course.name).to eq('New Name')
+      expect(course.author_id).to eq(user.id)
+      expect(course.learning_path_id).to eq(learning_path.id)
+      expect(course.order_in_learning_path).to eq(1)
     end
   end
 end
